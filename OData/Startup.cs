c98e8bb7adf403eb30ff907a2c53df15b5051898
Jianhan Zhu <zhuj@parliament.uk>
2018-11-20 11:27:00 +0000

    minor change

--- OData/Startup.cs
+++ OData/Startup.cs
@@ -45,7 +45,7 @@
             }
 
             //app.UseRewriter(new RewriteOptions().AddRewrite("^$", "swagger/index.html", false).AddRewrite("^(swagger|favicon)(.+)$", "swagger/$1$2", true));
-            app.UseRewriter(new RewriteOptions().AddRewrite("odata201710131103.azurewebsites.net", "api.parliament.uk/odata", false));
+            app.UseRewriter(new RewriteOptions().AddRewrite(@"^odata201710131103.azurewebsites.net", "api.parliament.uk/odata", true));
             //app.UseSwaggerUI(Startup.ConfigureSwaggerUI);
 
             var handler = new DefaultODataPathHandler(); // built-in
